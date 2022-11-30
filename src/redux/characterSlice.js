ae64ee0c05674b0e7417d4a6611b1ec86cc0014c
jona <jojhan91@gmail.com>
2022-11-30 18:18:12 -0500

    primer commit

---
+++ src/redux/characterSlice.js
@@ -0,0 +1,12 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+export const slice = createSlice({
+    name: 'characters',
+    initialState:{
+        characters : [],
+        datail:{}
+    },
+    reducer:"hola"
+
+});
